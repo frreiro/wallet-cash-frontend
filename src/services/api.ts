7253042a044f4161173a3b30a2bc6e0d2bea8b86
Lucas Viana <lucasvfv@gmail.com>
2022-11-21 20:08:44 -0300

    feat: docker added

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,6 @@
 import axios from 'axios';
-import { parseCookies } from 'nookies';
 
-const BASE_URL = 'http://localhost:5000';
+const BASE_URL = 'http://127.0.1:5000';
 
 
 const fetchAxios = axios.create({
