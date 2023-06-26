22a46f720d72883f03ad6d998d91c4ce0e736f2a
Lucas Viana <lucasvfv@gmail.com>
2023-06-26 13:30:19 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
 	swcMinify: true,
 	output:'standalone',
 	env: {
-		REQUEST_URL: 'https://ng-backend-0a0e.onrender.com',
+		REQUEST_URL: 'https://wallet-backend-a3tr.onrender.com',
 	}
 };
 
